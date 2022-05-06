fa3a8d8523c51db3b41679e4e5bef51a7b157aea
Noted Salmon <moreiraandre2001@gmail.com>
2022-05-06 03:39:50 +0100

    Added clsDataConnection that was missing and have a working Find method.

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -4,16 +4,112 @@ namespace ClassLibrary
 {
     public class clsCustomer
     {
-        public bool PaymentReceived { get; set; }
-        public DateTime DateAdded { get; set; }
-        public int CustomerID { get; set; }
-        public string HouseNo { get; set; }
-        public string PostCode { get; set; }
-        public string Street { get; set; }
-        public string Town { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Order { get; set; }
+        private Int32 mCustomerID;
+        private bool mCustomerPaymentReceived;
+        public bool CustomerPaymentReceived
+        {
+            get
+            {
+                return mCustomerPaymentReceived;
+            }
+            set
+            {
+                mCustomerPaymentReceived = value;
+            }
+        }
+        private DateTime mDateAdded;
+        public DateTime DateAdded
+        {
+            get
+            {
+                return mDateAdded;
+            }
+            set
+            {
+                mDateAdded = value;
+            }
+        
+        }
+        public int CustomerID {
+            get
+            {
+                return mCustomerID;
+            }
+            set
+            {
+                mCustomerID = value;
+            }
+         }
+        private string mCustomerAddress;
+        public string CustomerAddress
+        {
+            get
+            {
+                return mCustomerAddress;
+            }
+            set
+            {
+                mCustomerAddress = value;
+            }
+        }
+        private string mCustomerName;
+        public string CustomerName
+        {
+            get
+            {
+                return mCustomerName;
+            }
+            set
+            {
+                mCustomerName = value;
+            }
+        }
+        private string mCustomerEmail;
+        public string CustomerEmail
+        {
+            get
+            {
+                return mCustomerEmail;
+            }
+            set
+            {
+                mCustomerEmail = value;
+            }
+        }
+        private string mCustomerOrder;
+        public string CustomerOrder
+        {
+            get
+            {
+                return mCustomerOrder;
+            }
+            set
+            {
+                mCustomerOrder = value;
+            }
+        }
+
+        public bool Find(int CustomerID)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@CustomerID", CustomerID);
+            DB.Execute("sproc_tblCustomer_FilterByCustomerID");
+            if (DB.Count == 1)
+            {
+                mCustomerID = Convert.ToInt32(DB.DataTable.Rows[0]["CustomerID"]);
+                mCustomerAddress = Convert.ToString(DB.DataTable.Rows[0]["CustomerAddress"]);
+                mCustomerName = Convert.ToString(DB.DataTable.Rows[0]["CustomerName"]);
+                mCustomerEmail = Convert.ToString(DB.DataTable.Rows[0]["CustomerEmail"]);
+                mDateAdded = Convert.ToDateTime(DB.DataTable.Rows[0]["DateAdded"]);
+                mCustomerPaymentReceived = Convert.ToBoolean(DB.DataTable.Rows[0]["PaymentReceived"]);
+                mCustomerOrder = Convert.ToString(DB.DataTable.Rows[0]["CustomerOrder"]);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
